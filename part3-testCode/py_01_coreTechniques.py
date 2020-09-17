ece263cba11d8d03cdbb566ec526254fd2d14225
AuroraBoreas <AuroraBoreas@users.noreply.github.com>
2020-09-18 07:35:31 +0800

    chapter 03 stubs?

--- part3-testCode/py_01_coreTechniques.py
+++ part3-testCode/py_01_coreTechniques.py
@@ -1,7 +1,15 @@
 """
-
+P75
 ? concept: stubs
 using "stubs" to break dependencies
+
+A stub is a controllable replacement for an existing dependency (or collaborator) in the system. 
+By using a stub, you can test your code without dealing with the dependency directly. 
+
+? concept: external dependency
+An external dependency is an object in your system that your code under
+test interacts with, and over which you have no control. 
+(Common examples are filesystems, threads, memory, time, and so on.)
 
 ? concept: mock
 
