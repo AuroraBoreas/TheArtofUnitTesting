5bc91cd50ec05675a15019da75c8051d1106ea59
AuroraBoreas <AuroraBoreas@users.noreply.github.com>
2020-09-20 16:26:00 +0800

    interaction test

---
+++ part4-designAndProcess/py_01_usingMockObj.py
@@ -0,0 +1,12 @@
+"""
+
+? concept: interaction testing?
+put two or more units together, let em interact with each other,
+check outcome is what we expect. 
+
+? understanding Mock obj
+
+? difference btwn mock and stub?
+
+
+"""
